a9264a211e91f66a30c6fb93ec49524e3aec6f9a
JOKER95 <mixkud@inbox.ru>
2026-07-22 08:03:20 +0300

    feat: исправил  функцию loadIssues

--- issue-tracker/src/App.tsx
+++ issue-tracker/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState,  useMemo } from 'react'
+import { useState,  useMemo, useEffect } from 'react'
 import './App.css'
  
 
@@ -10,7 +10,7 @@ interface Comment {
   
 }
 interface Issue {
- id : number
+ id : number;
   title: string;
 status :string
 comments : Array<Comment>;
@@ -26,30 +26,37 @@ const myIssue : Issue = {
 id : 1,
 title : "React",
 status : "open",
-comments : [myComment] 
+comments : [myComment], 
 }
 
 function App() {
    const [issues, setIssue] = useState<Issue[]>([]);
    const [statusFilter, setstatusFilter] = useState<string>("open")
    const filterIssues = useMemo(() => issues.filter(item=> item.status === statusFilter ), [issues,statusFilter])
-   const loadissues= async ()=>{
+  useEffect(() => {
+  const loadissues = async () => {
     try {
-       const response =  await fetch("https://google.com")
-       const data = await response.json();
-        const  transformedIssues  = data.map((item: any) => {
-  return {
-    id: item.id,
-    title: item.title,
-    status: item.state, 
-    comments: []
-  };
-});
-  setIssue(transformedIssues);    
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await response.json();
+      console.log("СЫРЫЕ ДАННЫЕ ИЗ СЕТИ:", data);
+      const transfOne = data.map((item: any) => {
+        return {
+          id: item.id,
+          title: item.title,
+          status: item.status,
+          comments: []
+        };
+      });
+
+      console.log(transfOne);
+      setIssue(transfOne);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-   }
+  };
+
+  loadissues(); 
+}, []);
 function sortedIssues() {
  return filterIssues.slice().sort((a, b) => b.id- a.id); 
 }
